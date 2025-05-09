7d2901dbf67ce54381a028922938deac76428ce3
JenniferJun <jenniferjun.au@gmail.com>
2025-05-10 06:09:21 +0900

    The last Assignment for prisma

--- components/search-box.tsx
+++ components/search-box.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
-import { useCallback } from "react";
+import { useCallback, Suspense } from "react";
 
-export default function SearchBox() {
+function SearchBoxContent() {
     const router = useRouter();
     const searchParams = useSearchParams();
 
@@ -38,14 +38,34 @@ export default function SearchBox() {
                     className="flex-1 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
 
-
                 <button type="submit" className="primary-btn w-[50px] h-[40px] ">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
                     </svg>
                 </button>
-
             </div>
         </form>
+    );
+}
+
+export default function SearchBox() {
+    return (
+        <Suspense fallback={
+            <div className="flex gap-2">
+                <input
+                    type="search"
+                    placeholder="Search tweets..."
+                    className="flex-1 px-4 py-2 border rounded-lg"
+                    disabled
+                />
+                <button className="primary-btn w-[50px] h-[40px]" disabled>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                    </svg>
+                </button>
+            </div>
+        }>
+            <SearchBoxContent />
+        </Suspense>
     );
 } 
